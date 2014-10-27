e9e9612308d5160c35c449a3f467dfb62e4adc05
Marton Szeles <mlt.residence@gmail.com>
2014-10-27 22:08:39 +0000

    Generated new bundle called TopCarsBundle. The core of the project will live inside this bundle.

---
+++ src/Marton/TopCarsBundle/Controller/PageController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Marci
+ * Date: 27/10/14
+ * Time: 22:01
+ */
+
+namespace Marton\TopCarsBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class PageController extends Controller {
+
+} 
---
+++ web/bundles/martontopcars/js/marton.js
@@ -0,0 +1,3 @@
+/**
+ * Created by Marci on 27/10/14.
+ */
