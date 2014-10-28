91259bb4be2e9a76c7a0762ea73996d1e102d308
Marton Szeles <mlt.residence@gmail.com>
2014-10-28 21:21:42 +0000

    Added "Role" table. This will form a many-to-many relationship with "User" table.

---
+++ src/Marton/TopCarsBundle/Entity/Role.php
@@ -0,0 +1,95 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Marci
+ * Date: 28/10/14
+ * Time: 21:00
+ */
+
+namespace Marton\TopCarsBundle\Entity;
+
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Symfony\Component\Security\Core\Role\RoleInterface;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Table(name="tbl_role")
+ * @ORM\Entity()
+ */
+class Role implements RoleInterface {
+
+    /**
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id()
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(name="name", type="string", length=30)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(name="role", type="string", length=20, unique=true)
+     */
+    private $role;
+
+    // TODO: add many-to-many relationship with users
+    private $users;
+
+    public function __construct()
+    {
+        $this->users = new ArrayCollection();
+    }
+
+    /**
+     * @see RoleInterface
+     */
+    public function getRole()
+    {
+        return $this->role;
+    }
+
+    /**
+     * @param mixed $role
+     */
+    public function setRole($role)
+    {
+        $this->role = $role;
+    }
+
+    /**
+     * @param mixed $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+
+    /**
+     * Return the role field.
+     * @return string
+     */
+    public function __toString()
+    {
+        return (string) $this->role;
+    }
+
+}
