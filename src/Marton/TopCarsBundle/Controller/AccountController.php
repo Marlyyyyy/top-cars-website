6d8c18386a873b3a4c9b11a4afa6ac389952cb62
Marton Szeles <mlt.residence@gmail.com>
2014-10-29 02:24:37 +0000

    Added ability to register.

--- src/Marton/TopCarsBundle/Controller/AccountController.php
+++ src/Marton/TopCarsBundle/Controller/AccountController.php
@@ -13,6 +13,7 @@ use Marton\TopCarsBundle\Form\Model\Registration;
 use Marton\TopCarsBundle\Form\Type\RegistrationType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
 
 class AccountController extends Controller{
 
@@ -32,6 +33,51 @@ class AccountController extends Controller{
 
     public function createAction(Request $request)
     {
+        $em = $this->getDoctrine()->getManager();
+
+        $form = $this->createForm(new RegistrationType(), new Registration());
+
+        $form->handleRequest($request);
+
+        if ($form->isValid()) {
+
+            $role = $em->getRepository('MartonTopCarsBundle:Role')->findOneBy(array('role' => 'ROLE_USER'));
+
+            $registration = $form->getData();
+
+            $user = $registration->getUser();
+
+            // Encode the password of the user
+            $plainPassword = $user->getPassword();
+
+            $encoderFactory = $this->container->get('security.encoder_factory');
+            $encoder = $encoderFactory->getEncoder($user);
+
+            $encodedPassword = $encoder->encodePassword($plainPassword, $user->getSalt());
+            $user->setPassword($encodedPassword);
+
+            $user->addRole($role);
+
+            $em->persist($user);
+            $em->flush();
+
+            // Automatically log in the user after successful registration
+            $token = new UsernamePasswordToken($user, null, 'secured_area', $user->getRoles());
+            $this->get('security.context')->setToken($token);
+            $this->get('session')->set('_security_main',serialize($token));
+
+            $this->get('session')->getFlashBag()->add(
+                'notice',
+                'Welcome, now let the fun begin! :)'
+            );
+
+            return $this->redirect($this->generateUrl('marton_topcars_default'));
+        }
+
+        return $this->render(
+            'MartonTopCarsBundle:Default:Pages/registration.html.twig',
+            array('form' => $form->createView())
+        );
     }
 
 } 
