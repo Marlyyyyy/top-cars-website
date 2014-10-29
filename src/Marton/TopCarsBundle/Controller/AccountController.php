1ee0564c0cd0e45cb0148e266a7660fbad512833
Marton Szeles <mlt.residence@gmail.com>
2014-10-29 17:06:17 +0000

    Added ability to log in, along with the "Login" page.

--- src/Marton/TopCarsBundle/Controller/AccountController.php
+++ src/Marton/TopCarsBundle/Controller/AccountController.php
@@ -14,6 +14,7 @@ use Marton\TopCarsBundle\Form\Type\RegistrationType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
+use Symfony\Component\Security\Core\SecurityContextInterface;
 
 class AccountController extends Controller{
 
@@ -77,6 +78,35 @@ class AccountController extends Controller{
         return $this->render(
             'MartonTopCarsBundle:Default:Pages/registration.html.twig',
             array('form' => $form->createView())
+        );
+    }
+
+    public function loginAction(Request $request)
+    {
+
+        $session = $request->getSession();
+
+        // get the login error if there is one
+        if ($request->attributes->has(SecurityContextInterface::AUTHENTICATION_ERROR)) {
+            $error = $request->attributes->get(
+                SecurityContextInterface::AUTHENTICATION_ERROR
+            );
+        } elseif (null !== $session && $session->has(SecurityContextInterface::AUTHENTICATION_ERROR)) {
+            $error = $session->get(SecurityContextInterface::AUTHENTICATION_ERROR);
+            $session->remove(SecurityContextInterface::AUTHENTICATION_ERROR);
+        } else {
+            $error = '';
+        }
+
+        // Last username entered by the user
+        $lastUsername = (null === $session) ? '' : $session->get(SecurityContextInterface::LAST_USERNAME);
+
+        return $this->render(
+            'MartonTopCarsBundle:Default:Pages/login.html.twig',
+            array(
+                'last_username' => $lastUsername,
+                'error'         => $error,
+            )
         );
     }
 
