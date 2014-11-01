ecabb02e18c0a5acc93e0bce0238e3968ba9b9cf
Marton Szeles <mlt.residence@gmail.com>
2014-11-01 20:10:13 +0000

    Added Javascript function to preload images.

--- web/bundles/martontopcars/js/marton.js
+++ web/bundles/martontopcars/js/marton.js
@@ -1,3 +1,63 @@
 /**
  * Created by Marci on 27/10/14.
  */
+
+function preload_images(array, el){
+
+    var new_images = [], loaded_images = 0, arr_length = array.length, background_2, loading_container, progress_p;
+    var container = (typeof el === "undefined" ? document.body : el);
+
+    var post_action = function(){};
+
+    var arr = (typeof array != "object") ? [array] : array;
+
+    // Executes call-back function after preloading all images
+    function image_load_post(){
+
+        loaded_images++;
+        progress_p.innerHTML = Math.round(100*(loaded_images/arr_length)) + "%";
+        if (loaded_images == arr_length){
+            on_finish();
+            post_action(new_images);
+        }
+    }
+
+    // Creates loading screen
+    function on_create(){
+
+        loading_container = document.createElement("div");
+        loading_container.className  = "loading_container";
+
+        progress_p = document.createElement("p");
+        progress_p.className = "progress_p";
+        loading_container.appendChild(progress_p);
+
+        container.appendChild(loading_container);
+    }
+
+    // Removes loading screen
+    function on_finish(){
+
+        container.removeChild(loading_container);
+    }
+
+    on_create();
+
+    for (var i=0; i<arr_length; i++){
+        new_images[i] = new Image();
+        new_images[i].src = arr[i];
+        new_images[i].onload = function(){
+            image_load_post();
+        };
+        new_images[i].onerror = function(){
+            image_load_post();
+        }
+    }
+
+    // Return blank object with done() method
+    return {
+        done:function(f){
+            post_action= f || post_action;
+        }
+    }
+}
