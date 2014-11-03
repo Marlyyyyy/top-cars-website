07a864484802d1430a3d4d6b8ce3350d0cab2bfe
Marton Szeles <mlt.residence@gmail.com>
2014-11-03 22:59:37 +0000

    Added "SuggestedCar" table. This table will store temporary cars which were suggested by the user.

---
+++ src/Marton/TopCarsBundle/Entity/SuggestedCar.php
@@ -0,0 +1,175 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Marci
+ * Date: 03/11/14
+ * Time: 20:32
+ */
+
+namespace Marton\TopCarsBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="tbl_car_suggest")
+ * @ORM\HasLifecycleCallbacks()
+ */
+class SuggestedCar {
+
+    /**
+     * @ORM\Column(type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    protected $model;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    protected $image;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    protected $speed;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    protected $power;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    protected $torque;
+
+    /**
+     * @ORM\Column(type="float")
+     */
+    protected $acceleration;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    protected $weight;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    protected $comment;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    protected $createdAt;
+
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function setAcceleration($acceleration)
+    {
+        $this->acceleration = $acceleration;
+    }
+
+    public function getAcceleration()
+    {
+        return $this->acceleration;
+    }
+
+    public function setComment($comment)
+    {
+        $this->comment = $comment;
+    }
+
+    public function getComment()
+    {
+        return $this->comment;
+    }
+
+    public function setImage($image)
+    {
+        $this->image = $image;
+    }
+
+    public function getImage()
+    {
+        return $this->image;
+    }
+
+    public function setModel($model)
+    {
+        $this->model = $model;
+    }
+
+    public function getModel()
+    {
+        return $this->model;
+    }
+
+    public function setPower($power)
+    {
+        $this->power = $power;
+    }
+
+    public function getPower()
+    {
+        return $this->power;
+    }
+
+    public function setSpeed($speed)
+    {
+        $this->speed = $speed;
+    }
+
+    public function getSpeed()
+    {
+        return $this->speed;
+    }
+
+    public function setTorque($torque)
+    {
+        $this->torque = $torque;
+    }
+
+    public function getTorque()
+    {
+        return $this->torque;
+    }
+
+    public function setWeight($weight)
+    {
+        $this->weight = $weight;
+    }
+
+    public function getWeight()
+    {
+        return $this->weight;
+    }
+
+    /**
+     * @ORM\PrePersist
+     */
+    public function setCreatedAt()
+    {
+        $this->createdAt = new \DateTime();
+    }
+
+    public function getCreatedAt()
+    {
+        return $this->createdAt;
+    }
+} 
