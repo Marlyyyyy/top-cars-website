c65764c81d632e53ef8bf416b5aca21e48153b5d
Marton Szeles <mlt.residence@gmail.com>
2014-11-18 21:14:18 +0000

    Modified "DataController" to only contain one method that takes care of executing one-time tasks depending on the argument.

--- src/Marton/TopCarsBundle/Controller/DataController.php
+++ src/Marton/TopCarsBundle/Controller/DataController.php
@@ -16,19 +16,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DataController extends Controller{
 
-    public function addPricesToCarsAction(){
+    public function oneTimeTaskAction($task){
 
         /* @var $repository CarRepository */
         $repository = $this->getDoctrine()->getRepository('MartonTopCarsBundle:Car');
         $cars = $repository-> findAllCars();
 
         $priceCalculator = new PriceCalculator();
-
-        foreach ($cars as $car){
-            /* @var $car Car */
-            $id = $car->getId();
-            $repository->updatePriceWhereId($id, $priceCalculator->calculatePrice($car));
-        }
 
         return $this->render('MartonTopCarsBundle:Default:Pages/home.html.twig');
     }
