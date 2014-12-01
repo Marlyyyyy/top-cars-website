bb1488903509fd0052ad68b20b9855d999db51e7
Marton Szeles <mlt.residence@gmail.com>
2014-12-01 16:40:43 +0000

    Added custom error messages for data transformation failures within the "SuggestedCar" model.

--- src/Marton/TopCarsBundle/Form/Type/SuggestedCarType.php
+++ src/Marton/TopCarsBundle/Form/Type/SuggestedCarType.php
@@ -18,13 +18,13 @@ class SuggestedCarType extends AbstractType{
     public function buildForm(FormBuilderInterface $builder, array $options){
 
         $builder
-            ->add('model', 'text', array('error_bubbling' => true))
-            ->add('image_file', 'file', array('required'=>false), array('error_bubbling' => true))
-            ->add('speed', 'number', array('error_bubbling' => true))
-            ->add('power', 'number', array('error_bubbling' => true))
-            ->add('torque', 'number', array('error_bubbling' => true))
-            ->add('acceleration', 'number', array('error_bubbling' => true))
-            ->add('weight', 'number', array('error_bubbling' => true))
+            ->add('model', 'text', array('invalid_message' => 'The name of the model has to be text!'), array('error_bubbling' => true))
+            ->add('image_file', 'file', array('required'=>false, 'invalid_message' => 'The image cannot be greater than 1MB!'), array('error_bubbling' => true))
+            ->add('speed', 'number', array('invalid_message' => 'Speed has to be a number!'), array('error_bubbling' => true))
+            ->add('power', 'number', array('invalid_message' => 'Power has to be a number!'), array('error_bubbling' => true))
+            ->add('torque', 'number', array('invalid_message' => 'Torque has to be a number!'), array('invalid_message' => 'The name of the model has to be text!'), array('error_bubbling' => true))
+            ->add('acceleration', 'number', array('invalid_message' => 'Acceleration has to be a number!'), array('error_bubbling' => true))
+            ->add('weight', 'number', array('invalid_message' => 'Weight has to be a number!'), array('error_bubbling' => true))
             ->add('comment', 'textarea', array('error_bubbling' => true))
             ->add('save', 'submit', array('label' => 'Submit'), array('error_bubbling' => true));
     }
