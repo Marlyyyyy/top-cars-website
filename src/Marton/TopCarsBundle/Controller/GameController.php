a7cf49b13240057ee55907874b508aa09009256a
Marton Szeles <mlt.residence@gmail.com>
2014-12-01 12:06:27 +0000

    Refactoring: Moved a method responsible for updating the user's score within the game to a separate controller. This improves the logic related to controllers and models.

--- src/Marton/TopCarsBundle/Controller/GameController.php
+++ src/Marton/TopCarsBundle/Controller/GameController.php
@@ -14,6 +14,8 @@ use Marton\TopCarsBundle\Entity\User;
 use Marton\TopCarsBundle\Entity\UserProgress;
 use Marton\TopCarsBundle\Repository\CarRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class GameController extends Controller{
 
@@ -54,5 +56,65 @@ class GameController extends Controller{
             "user_deck" => json_encode($selected_cars),
             "user_level_info" => json_encode($user_level_info)
         ));
+    }
+
+    // Ajax call to update the user's score during a game
+    public function postUserScoreAction(Request $request){
+
+        // Get entity manager
+        $em = $this->getDoctrine()->getManager();
+
+        // Get user entity
+        /* @var $user User */
+        $user= $this->get('security.context')->getToken()->getUser();
+        $progress = $user->getProgress();
+
+        // Score and Level
+        $score = (int) $request->request->get('score');
+
+        $achievementCalculator = new AchievementCalculator();
+        $new_score_info = $achievementCalculator->calculateLevel($score);
+
+        /* @var $progress UserProgress */
+        $progress->setScore($score);
+
+        $old_level = $progress->getLevel();
+        $progress->setLevel($new_score_info["level"]);
+
+        // Streak
+        $streak = (int) $request->request->get('streak');
+        $old_streak = $progress->getStreak();
+        if ($streak > $old_streak) $progress->setStreak($streak);
+
+        // Round Result
+        $roundResult = $request->request->get('roundResult');
+        $old_allRound = $progress->getAllRound();
+        $progress->setAllRound($old_allRound + 1);
+        switch ($roundResult){
+            case "win":
+                $progress->setRoundWin($progress->getRoundWin()+1);
+                break;
+            case "lose":
+                $progress->setRoundLose($progress->getRoundLose()+1);
+                break;
+        }
+
+        if ($old_level<$new_score_info["level"]){
+            $level_change = "up";
+            $progress->setGold($progress->getGold() + $achievementCalculator->calculateGold($progress->getLevel()));
+        }else if($old_level===$new_score_info["level"]){
+            $level_change = "stay";
+        }else{
+            $level_change = "down";
+        }
+
+        $em->flush();
+
+        $response = new Response(json_encode(array(
+            'levelChange' => $level_change,
+            'userLevelInfo' => $new_score_info)));
+        $response->headers->set('Content-Type', 'application/json');
+
+        return $response;
     }
 } 
