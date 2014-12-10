bcb5bcd8dc6cd26ea9f5e22e90976b3b45e8345e
Marton Szeles <mlt.residence@gmail.com>
2014-12-10 14:31:16 +0000

    Added timezone setting to the Kernel in order to prevent crashing on Dice computers.

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -5,6 +5,14 @@ use Symfony\Component\Config\Loader\LoaderInterface;
 
 class AppKernel extends Kernel
 {
+
+    // This constructor is to prevent crashing when relying on the system's timezone setting
+    public function __construct($environment, $debug)
+    {
+        date_default_timezone_set( 'Europe/Paris' );
+        parent::__construct($environment, $debug);
+    }
+
     public function registerBundles()
     {
         $bundles = array(
